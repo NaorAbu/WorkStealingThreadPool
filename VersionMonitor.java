ac639aacb6be7f0155a5bbb2c9e96f49a818b79a
NaorAbu <35075782+NaorAbu@users.noreply.github.com>
2018-05-27 14:17:48 +0300

    Update VersionMonitor.java

--- VersionMonitor.java
+++ VersionMonitor.java
@@ -6,15 +6,6 @@ package WorkStealingThreadPool;
  * {@link #getVersion()} once you have a version number, you can call
  * {@link #await(int)} with this version number in order to wait until this
  * version number changes.
- * <p>
- * you can also increment the version number by one using the {@link #inc()}
- * method.
- * <p>
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add can only be
- * private, protected or package protected - in other words, no new public
- * methods
  */
 public class VersionMonitor {
     private int version = 0;
