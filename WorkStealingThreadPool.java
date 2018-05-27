8256960d13177f3484a726b60cead3a78550adb3
NaorAbu <35075782+NaorAbu@users.noreply.github.com>
2018-05-27 14:18:15 +0300

    Update WorkStealingThreadPool.java

--- WorkStealingThreadPool.java
+++ WorkStealingThreadPool.java
@@ -6,12 +6,6 @@ import java.util.Random;
 /**
  * represents a work stealing thread pool - to understand what this class does
  * please refer to your assignment.
- * <p>
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add can only be
- * private, protected or package protected - in other words, no new public
- * methods
  */
 public class WorkStealingThreadPool {
     protected VersionMonitor vm;
@@ -24,11 +18,6 @@ public class WorkStealingThreadPool {
      * creates a {@link WorkStealingThreadPool} which has nthreads
      * {@link Processor}s. Note, threads should not get started until calling to
      * the {@link #start()} method.
-     * <p>
-     * Implementors note: you may not add other constructors to this class nor
-     * you allowed to add any other parameter to this constructor - changing
-     * this may cause automatic tests to fail..
-     *
      * @param nthreads the number of threads that should be started by this
      *                 thread pool
      */
@@ -71,9 +60,6 @@ public class WorkStealingThreadPool {
      * closes the thread pool - this method interrupts all the threads and wait
      * for them to stop - it is returns *only* when there are no live threads in
      * the queue.
-     * <p>
-     * after calling this method - one should not use the queue anymore.
-     *
      * @throws InterruptedException          if the thread that shut down the threads is
      *                                       interrupted
      * @throws UnsupportedOperationException if the thread that attempts to
