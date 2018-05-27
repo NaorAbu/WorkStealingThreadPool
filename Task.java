85f2531e5b3511afc02f7bdbb2f62652693ac5a3
NaorAbu <35075782+NaorAbu@users.noreply.github.com>
2018-05-27 14:17:31 +0300

    Update Task.java

--- Task.java
+++ Task.java
@@ -7,12 +7,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * an abstract class that represents a task that may be executed using the
  * {@link WorkStealingThreadPool}
- * <p>
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add to this class can
- * only be private!!!
- *
  * @param <R> the task result type
  */
 public abstract class Task<R> {
@@ -68,10 +62,6 @@ public abstract class Task<R> {
     /**
      * add a callback to be executed once *all* the given tasks results are
      * resolved
-     * <p>
-     * Implementors note: make sure that the callback is running only once when
-     * all the given tasks completed.
-     *
      * @param tasks
      * @param callback the callback to execute once all the results are resolved
      */
